5cbab6fecc37ee37034a2868febf88dfbc427778
Esequiel <virtuosoesequiel@gmail.com>
2021-07-19 14:14:34 -0300

    Fix lint issues.

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Ingredient, User
+from core.models import Ingredient
 
 from recipe.serializers import IngredientSerializer
 
